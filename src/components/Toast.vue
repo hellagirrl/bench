7b45dfd5988dfba81fbe3ece40f1d0b31aff1e64
Kat <catherineitj28@gmail.com>
2021-12-24 18:58:50 +0300

    more changes

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -2,7 +2,7 @@
   <div aria-live="assertive" aria-atomic="true">
     <div
       id="liveToast"
-      class="toast toast-notification show position-fixed"
+      class="toast shadow-none toast-notification show position-fixed"
       role="alert"
     >
       <div class="alert-danger toast-body">
