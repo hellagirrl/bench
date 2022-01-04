3174bf98009359eec8f29eced200ab64972585f1
Kat <catherineitj28@gmail.com>
2022-01-04 14:56:07 +0300

    updated styling

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,53 +5,57 @@
     >
       <a href="/" class="navbar-brand">Benchkiller</a>
     </nav>
-    <main class="container-xl">
+    <main class="container pt-5">
       <Toast v-if="showToast" />
       <div class="row mx-n3">
-        <div class="col-md-6 form px-3">
-          <form class="form mb-3" @submit.prevent="onSubmit">
-            <div class="mb-3">
-              <label for="userTelegram" class="form-label">{{
-                $t("message.nickname")
-              }}</label>
-              <input
-                type="text"
-                class="form-control"
-                id="userTelegram"
-                v-model="userTelegram"
-              />
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-md-6 form px-3">
+              <form class="form mb-3" @submit.prevent="onSubmit">
+                <div class="mb-3">
+                  <label for="userTelegram" class="form-label">{{
+                    $t("message.nickname")
+                  }}</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="userTelegram"
+                    v-model="userTelegram"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="userPassword" class="form-label">{{
+                    $t("message.password")
+                  }}</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="userPassword"
+                    v-model="userPassword"
+                  />
+                </div>
+                <button type="submit" id="liveToastBtn" class="btn btn-success">
+                  {{ $t("message.loginButton") }}
+                </button>
+              </form>
+              <div class="alert alert-primary">
+                <span>{{ $t("message.alertMessage1") }}</span>
+                <a
+                  class="text-decoration-none"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://t.me/benchkiller_test_bot"
+                  >{{ $t("message.botLink") }}</a
+                >
+                {{ $t("message.alertMessage2") }}
+              </div>
             </div>
-            <div class="mb-3">
-              <label for="userPassword" class="form-label">{{
-                $t("message.password")
-              }}</label>
-              <input
-                type="password"
-                class="form-control"
-                id="userPassword"
-                v-model="userPassword"
-              />
-            </div>
-            <button type="submit" id="liveToastBtn" class="btn btn-success">
-              {{ $t("message.loginButton") }}
-            </button>
-          </form>
-          <div class="alert alert-primary" role="alert">
-            <span>{{ $t("message.alertMessage1") }}</span>
-            <a
-              class="text-decoration-none"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://t.me/benchkiller_test_bot"
-              >{{ $t("message.botLink") }}</a
-            >
-            {{ $t("message.alertMessage2") }}
           </div>
         </div>
       </div>
     </main>
     <footer class="footer pb-3">
-      <div class="container-xl">
+      <div class="container">
         © Benchkiller 2021<span v-if="currentYear > 2021">
           - {{ currentYear }}</span
         >
