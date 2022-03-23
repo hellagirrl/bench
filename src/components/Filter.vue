c285af200519a0914ad998cfeba8e3d53c781a26
Kat <catherineitj28@gmail.com>
2022-03-23 17:25:21 +0300

    month format fix

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -110,7 +110,21 @@ export default {
 
     const format = (date) => {
       const day = date.getDate();
-      const month = date.getMonth() + 1;
+      const months = [
+        '01',
+        '02',
+        '03',
+        '04',
+        '05',
+        '06',
+        '07',
+        '08',
+        '09',
+        '10',
+        '11',
+        '12',
+      ];
+      const month = months[date.getMonth()];
       const year = date.getFullYear();
       return `${day}.${month}.${year}`;
     };
